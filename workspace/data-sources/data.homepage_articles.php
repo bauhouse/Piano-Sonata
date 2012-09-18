ac6a5eb0f3a9a1ad10c2a3304d9e7d0d829601dc
Stephen Bau <stephen@domain7.com>
2012-09-18 07:07:52 -0700

    Update Article and Homepage Articles data sources to include Subsection Manager entries for the Article Images field

--- workspace/data-sources/data.homepage_articles.php
+++ workspace/data-sources/data.homepage_articles.php
@@ -1,68 +1,62 @@
 <?php
 
 	require_once(TOOLKIT . '/class.datasource.php');
-	
-	Class datasourcehomepage_articles extends Datasource{
-		
+
+	Class datasourcehomepage_articles extends SectionDatasource{
+
 		public $dsParamROOTELEMENT = 'homepage-articles';
 		public $dsParamORDER = 'desc';
+		public $dsParamPAGINATERESULTS = 'yes';
 		public $dsParamLIMIT = '3';
-		public $dsParamREDIRECTONEMPTY = 'no';
-		public $dsParamPARAMOUTPUT = 'system:id';
-		public $dsParamSORT = 'date';
 		public $dsParamSTARTPAGE = '1';
+		public $dsParamREDIRECTONEMPTY = 'no';
+		public $dsParamPARAMOUTPUT = array(
+				'system:id'
+		);
+		public $dsParamSORT = 'date';
+		public $dsParamASSOCIATEDENTRYCOUNTS = 'yes';
 		
+
 		public $dsParamFILTERS = array(
 				'5' => 'yes',
 		);
 		
+
 		public $dsParamINCLUDEDELEMENTS = array(
 				'title',
 				'body: formatted',
 				'date',
 				'manage-images',
-				'article-images',
+				'article-images: image',
+				'article-images: description',
 				'categories',
 				'tags'
 		);
+		
 
-		public function __construct(&$parent, $env=NULL, $process_params=true){
-			parent::__construct($parent, $env, $process_params);
+		public function __construct($env=NULL, $process_params=true){
+			parent::__construct($env, $process_params);
 			$this->_dependencies = array();
 		}
-		
+
 		public function about(){
 			return array(
-					 'name' => 'Homepage Articles',
-					 'author' => array(
-							'name' => 'Fazal Khan',
-							'website' => 'http://fazalkhan.co.uk',
-							'email' => 'faz888@gmail.com'),
-					 'version' => '1.0',
-					 'release-date' => '2009-10-21T15:38:48+00:00');	
+				'name' => 'Homepage Articles',
+				'author' => array(
+					'name' => 'Stephen Bau',
+					'website' => 'http://home/sym/ensembles/piano-sonata-2.3',
+					'email' => 'bauhouse@gmail.com'),
+				'version' => 'Symphony 2.3',
+				'release-date' => '2012-09-18T13:49:29+00:00'
+			);
 		}
-		
+
 		public function getSource(){
 			return '1';
 		}
-		
+
 		public function allowEditorToParse(){
 			return true;
 		}
-		
-		public function grab(&$param_pool){
-			$result = new XMLElement($this->dsParamROOTELEMENT);
-				
-			try{
-				include(TOOLKIT . '/data-sources/datasource.section.php');
-			}
-			catch(Exception $e){
-				$result->appendChild(new XMLElement('error', $e->getMessage()));
-				return $result;
-			}	
 
-			if($this->_force_empty_result) $result = $this->emptyXMLSet();
-			return $result;
-		}
 	}
-
